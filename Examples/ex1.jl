956f32ccca1241d66baa7ef7e2f882241ba7e482
rh60 <hlavickar@gmail.com>
2022-06-24 10:28:05 +0200

    monoms

--- Examples/ex1.jl
+++ Examples/ex1.jl
@@ -1,9 +1,13 @@
 using Polynomials
-using MultivariatePolynomials
+using TypedPolynomials
+using LinearAlgebra
 
-p=Polynomial([1,2,3])
+p=Polynomials.Polynomial([1,2,3])
 P=integrate(p)
 P(1)-P(0)
 
+@polyvar x y
 
-
+monoms=TypedPolynomials.monomials((x, y), 0:2)
+p=monoms[2]
+p(x=>2,y=>3)

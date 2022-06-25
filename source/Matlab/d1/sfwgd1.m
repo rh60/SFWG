231b2c3d37a7127050a0d7ae5afc3c81ddc69f0d
Rudolf <Rudolf@DESKTOP-G547SSF>
2022-06-25 15:17:42 +0200

    order

--- source/Matlab/d1/sfwgd1.m
+++ source/Matlab/d1/sfwgd1.m
@@ -3,21 +3,15 @@ classdef sfwgd1
     % solver class dim=1
 
     properties
-        D
-        domain
-        x
-        loc
-        K, w        
-        sol
-        h, k
-        monoms
-        f
+        M, D, k, h, monoms, domain, f        
+        x, K, w
+        sol, lift                
     end
 
     methods
         function obj = sfwgd1(problem,polyorder)
-            [M,obj.monoms] = sfwgd1.MassMatrix(polyorder+1,1);
-            obj.D = M^(-1);
+            [obj.M,obj.monoms] = sfwgd1.MassMatrix(polyorder+1,1);
+            obj.D = obj.M^(-1);
             obj.h = 1;
             obj.k = polyorder;
             obj.domain = problem.domain;
@@ -25,21 +19,22 @@ classdef sfwgd1
             obj.w = zeros(polyorder+2, polyorder+3);
             obj.K = zeros(polyorder+3);
         end
-        
+
         % u0 is polynomial
         function w = weakdiff(obj,u1,u0,u2)
+            L2 = @sfwgd1.L2product;
             n = size(obj.D,1);
-            b = zeros(n,1);
-            q = 1;
+            b = zeros(n,1);            
             for i=1:n
-                b(i) = - sfwgd1.L2product(u0,polyder(q),obj.h) + ...
-                    u2*polyval(q,obj.h)-u1*polyval(q,0);
-                q = [q 0];
+                q = obj.monoms{i};
+                b(i) = - L2(u0,polyder(q),obj.h) + ...
+                    u2*polyval(q,obj.h)-u1*polyval(q,0);                
             end
             w = obj.D*b;
-        end    
+        end
 
         function obj = solve(obj,nele)
+            L2 = @sfwgd1.L2product;
             obj.x = linspace( obj.domain(1),obj.domain(2),nele+1);
             obj.w(:,1) = obj.weakdiff(1,0,0);
             p=1;
@@ -52,12 +47,12 @@ classdef sfwgd1
             n = size(obj.K,1);
             for i = 1:n
                 for j = 1:i
-                    obj.K(i,j) = sfwgd1.L2product(obj.w(:,i).',obj.w(:,j).',obj.h);
+                    obj.K(i,j) = L2(obj.w(:,i).',obj.w(:,j).',obj.h);
                 end
             end
             obj.K=obj.K+tril(obj.K,-1)';
             N = nele*(obj.k+1) + nele-1;
-            G = sparse(N,N);            
+            G = sparse(N,N);
             G(1:n-1,1:n-1) = obj.K(2:n,2:n);
             ins = n-1;
             for i = 2:nele-1
@@ -70,30 +65,62 @@ classdef sfwgd1
             ins = 1;
             for i=1:nele
                 for j=1:obj.k+1
-                    rhs(ins) = sfwgd1.L2product(obj.f,obj.monoms{j},obj.h);
+                    rhs(ins) = L2(obj.f,obj.monoms{j+1},obj.h); % !!!!!!!!
                     ins = ins+1;
                 end
                 ins=ins+1;
             end
-            obj.sol = G\rhs;
+            obj.sol = [0; G\rhs; 0];
+            obj = Lift(obj);
+        end
+
+        function obj = Lift(obj)
+            L2 = @sfwgd1.L2product;
+            nele = numel(obj.x)-1;
+            m = obj.monoms{1};
+            mon = [ [m 0] obj.monoms ];
+            Dmon = cellfun(@polyder, mon(1:end-1), 'UniformOutput',false);
+            n = numel(Dmon); 
+            A = zeros(n); b = zeros(n,1);
+            for i=1:n
+                for j=1:n
+                    A(i,j) = L2(Dmon{i},Dmon{j},obj.h);
+                end
+            end
+            obj.lift = zeros(nele,obj.k+3);
+            for i=1:nele
+                u = obj.sol((i-1)*n+1:i*n+1);
+                w=obj.w*u;
+                % w=obj.weakdiff(obj.sol(1),obj.sol(2),obj.sol(3))
+                for j=1:n
+                    b(j) = L2(w',Dmon{j},obj.h);
+                end
+                uh = A\b;
+                uh = [uh' 0];
+                u0 = u(2:end-1);
+                uh(end) = (L2(u0,1,obj.h)-L2(uh,1,obj.h))/L2(1,1,obj.h);
+                obj.lift(i,:)=uh;
+            end            
         end
     end
 
     methods (Static)
-        function [M,mon]=MassMatrix(k,h)
+        function [M,mon] = MassMatrix(k,h)
+            L2 = @sfwgd1.L2product;
             M = zeros(k+1);
-            mon = cell(1,k+1); 
+            mon = cell(1,k+1);
             mon{1}=1;
             for i=2:k+1
                 mon{i} = [mon{i-1} 0];
             end
-            for i=1:k+1               
-                for j=1:i                    
-                    M(i,j)=sfwgd1.L2product(mon{i},mon{j},h);                    
-                end                
+            mon=flip(mon);
+            for i=1:k+1
+                for j=1:i
+                    M(i,j) = L2(mon{i},mon{j},h);
+                end
             end
             M=M+tril(M,-1)';
-            M=flip(M,2);
+            %M=flip(M,2);
         end
 
         function r=L2product(p,q,h)
--- source/Matlab/d1/tst.m
+++ source/Matlab/d1/tst.m
@@ -3,5 +3,9 @@ problem.domain=[0 2];
 problem.f=1;
 s=sfwgd1(problem,0);
 s=s.solve(2);
-s.sol
+s.lift
+x=linspace(0,1);
+plot(x,polyval(s.lift(1,:),x),x+1,polyval(s.lift(1,:),x+1))
+
+
 
